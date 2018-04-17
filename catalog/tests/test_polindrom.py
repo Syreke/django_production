6b253aed66e7909c9e36e0800a6d4a321fc64fe7
Syrym Seydazimov <syreken.ss@gmail.com>
2018-04-17 23:36:23 +0600

    First version of application moved into github

---
+++ catalog/tests/test_polindrom.py
@@ -0,0 +1,28 @@
+﻿from django.test import TestCase
+
+# Create your tests here.
+
+from catalog.models import Author
+
+class TestPolindrom(TestCase):
+
+    
+
+    def is_palindrom(self):
+        author='kazakh'
+        author2=author[::-1]
+        x = len(author)
+        i = 0
+        x = x - 1
+        k = 0
+        while x - i >= i:
+            if author[x - i] == author[i]:
+                i += 1
+            else:
+                k = 1
+            break
+        if k == 1:
+            print("False")
+        else:
+            print("True")
+        self.assertEquals(author, author2)
